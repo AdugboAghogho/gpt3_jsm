24b4fc549a358f015bea45c29f6fc27f47632f33
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-26 17:58:59 +0100

    done with whatgpt3

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,9 +3,9 @@ import './features.css'
 
 const Features = () => {
   return (
-    <div>
-      Feature
-    </div>
+    <div className="gpt3__features-heading">
+        <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+      </div>
   )
 }
 
