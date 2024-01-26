dc849da1cc92d1c78997b2b7f961e495f55e7a80
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-26 11:32:24 +0100

    logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
+          <p><a href="#possibillty">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+
+        <div className="gpt3__navbar-sign"></div>
+      </div>
     </div>
   )
 }
