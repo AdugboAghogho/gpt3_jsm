c47c2f8b560ac7e22415249d100d8fb8607887af
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-26 11:15:00 +0100

    starting Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import { CTA, Brand, Navbar } from './components'
@@ -6,7 +7,7 @@ import { CTA, Brand, Navbar } from './components'
 const App = () => {
   return (
     <div className='App'>
-      <div className="gradinet__bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
