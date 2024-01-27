7ead6b7340a4be5d6933d6bf98bc7b906fc7dc71
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-27 18:13:00 +0100

    second commit

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,9 +3,16 @@ import './features.css'
 
 const Features = () => {
   return (
-    <div className="gpt3__features-heading">
+    <div className='gpt__features section__padding' id='features'>
+      <div className="gpt3__features-heading">
         <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
       </div>
+
+      <div className="gpt3__features-containers">
+        .
+      </div>
+    </div>
   )
 }
 
