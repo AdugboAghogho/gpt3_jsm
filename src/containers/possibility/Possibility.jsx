b585c22be0b23416b5a5b85d717bfcd437f1efb5
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-27 21:59:42 +0100

    ttt

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './possibility.css'
-import possibility  from '../../assets/possibility'
+import possibility  from '../../assets/possibility.png'
 
 const Possibility = () => {
   return (
@@ -11,7 +11,7 @@ const Possibility = () => {
 
       <div className="gpt3__possibility-content">
         <h4>Request Early Access to Get Started</h4>
-        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not 
           thoughts all exercise blessing. Indulgence way everything joy 
           alteration boisterous the attachment. Party we years to order allow 
