bf535bdafecaff7c78ec2a7f3a1a2048204211fb
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-29 11:44:48 +0100

    blod going

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -12,14 +12,14 @@ const Blog = () => {
 
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          <Article />
+          <Article imgUrl={blog01} date='Sep 26, 2024' title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
         </div>
 
         <div className="gpt3__blog-container_groupB">
-          <Article />
-          <Article />
-          <Article />
-          <Article />
+          <Article imgUrl={blog1}  date='Sep 26, 2024' title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog2}  date='Sep 26, 2024' title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog3}  date='Sep 26, 2024' title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog4}  date='Sep 26, 2024' title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
         </div>
       </div>
     </div>
