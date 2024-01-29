fba27eacfee1535b022e6062e83c4d97ab39a258
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-29 10:39:23 +0100

    cta done

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -7,7 +7,10 @@ const CTA = () => {
       <div className="gpt3__cta-content">
         <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possiblities.</h3>
-        <button className='gpt3__cta-btn' type='button'>Get Started</button>
+      </div>
+
+      <div className="gpt3__cta-btn">
+        <button type='button'>Get Started</button>
       </div>
     </div>
   )
