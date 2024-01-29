3b492d2177e0c875fdc79165f49acfa21fa50ae6
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-29 11:13:06 +0100

    blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -3,8 +3,10 @@ import './blog.css'
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className="bpt3__blog section__padding" id='blog'>
+      <div className="gpt3__blog-heading">
+        <h1 className=" gradinet__text">A lot is happening, We are blogging about it.</h1>
+      </div>
     </div>
   )
 }
